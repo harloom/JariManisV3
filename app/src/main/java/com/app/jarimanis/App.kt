f8f4f91b7ce1973d8761bddb068e6cd618d8e9da
Harloom <harloom.developer@gmail.com>
2019-09-24 22:32:20 +0700

    1.notifcation channel

--- app/src/main/java/com/app/jarimanis/App.kt
+++ app/src/main/java/com/app/jarimanis/App.kt
@@ -1,9 +1,19 @@
 package com.app.jarimanis
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+
+import android.os.Build
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+
 import com.app.jarimanis.data.datasource.local.TokenUser
 import com.app.jarimanis.di.NetworkModule
 import com.app.jarimanis.di.appModule
+
+import com.app.jarimanis.utils.NotificationID.createChannel
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -12,7 +22,7 @@ class App : Application(){
     override fun onCreate() {
         super.onCreate()
         TokenUser.init(this@App)
-
+        createChannel(this@App)
 
         startKoin {
             androidLogger()
@@ -20,4 +30,7 @@ class App : Application(){
             modules(listOf(NetworkModule, appModule))
         }
     }
+
+
+
 }
---
+++ app/src/main/java/com/app/jarimanis/utils/NotificationID.kt
@@ -0,0 +1,48 @@
+package com.app.jarimanis.utils
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.content.ContextCompat.getSystemService
+
+object NotificationID {
+
+        const  val CHANNEL_NAME1: String = "Channel Notif"
+        const val CHANNEL_ID1: String = "JariManis C1"
+        const val CHANNEL_DESC1 : String = "Pemberitahuan"
+
+
+        const val CHANNEL_NAME2: String = "Channel Message"
+        const val CHANNEL_ID2: String = "JariManis C1"
+        const val CHANNEL_DESC2 : String = "Pemberitahuan Pesan"
+
+
+
+        val GROUP_MESSAGE = "com.android.jarimanis.message"
+
+        fun createChannel(context: Context){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        val importance = NotificationManager.IMPORTANCE_HIGH
+                        val channel_notif = NotificationChannel(CHANNEL_ID1, CHANNEL_NAME1, importance).apply {
+                                description = CHANNEL_DESC1
+                        }
+                        // Register the channel with the system
+                        val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+                        val channel_message = NotificationChannel(CHANNEL_ID2, CHANNEL_NAME2, importance).apply {
+                                description = CHANNEL_DESC2
+                        }
+
+
+
+                        notificationManager.createNotificationChannel(channel_notif)
+                        notificationManager.createNotificationChannel(channel_message)
+
+
+                }
+        }
+
+
+
+}
