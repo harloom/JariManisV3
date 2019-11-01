435ceca8f6dc5fd3bb722ff74f179fd71b0ce5ae
harloom <harloom.developer@gmail.com>
2019-11-01 16:50:19 +0700

    1.pemberitahuan onclick building 50%

--- app/src/main/java/com/app/jarimanis/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/app/jarimanis/ui/notifications/NotificationsFragment.kt
@@ -5,18 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.app.jarimanis.R
 import com.app.jarimanis.data.datasource.models.pemberitahuan.Doc
 import com.app.jarimanis.data.repository.pemberitahuan.PemberitahuanModelFactory
+import com.app.jarimanis.utils.Key.THREAD
+import com.app.jarimanis.utils.Key.THREADID
 import kotlinx.android.synthetic.main.fragment_notifications.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.get
 
 class NotificationsFragment : Fragment(), PemberitahuanListAdapter.Interaction {
+    private var jobOnclick : Job?=null
     override fun onItemSelected(position: Int, item: Doc) {
-
+//        jobOnclick?.cancel()
+//        jobOnclick = CoroutineScope(Main).launch {
+//            delay(200)
+//            val bundleof = bundleOf(THREADID to item.threadId!!.id)
+//            findNavController().navigate(R.id.action_navigation_notifications_to_threadDetailFragment2,bundleof)
+//        }
     }
 
     private lateinit var notificationsViewModel: PemberitahuanViewModel
